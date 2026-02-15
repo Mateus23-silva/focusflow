805453ef095e396c9d97b1f7452a1eb093756edf
Mateus Miranda <85886971+Mateus23-silva@users.noreply.github.com>
2026-02-15 17:29:50 -0300

    implementa logica inicial com manipulacao do DOM

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("FocusFlow iniciado!");
+const input = document.getElementById("task-Input");
+const button = document.getElementById("addButton");
+const list = document.getElementById("taskList");
+
+let tasks = [];
+
+button.addEventListener("click", addTask);
+
+function addTask() {
+    const taskText = input.value;
+
+    if (taskText === "") return;
+
+    tasks.push(taskText);
+
+    input.value = "";
+
+    renderTasks();
+}
+
+function renderTasks() {
+    list.innerHTML = "";
+
+    tasks.forEach(function(task) {
+        const li = document.createElement("li");
+        li.textContent = task;
+
+        list.appendChild(li);
+    })
+}

30bd4494c31c69eb9f31555429cb058708f387b9
Mateus Miranda <85886971+Mateus23-silva@users.noreply.github.com>
2026-02-16 20:41:56 -0300

    Atualizações no projeto e ajustes de funcionamento

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.getElementById("task-Input");
 const button = document.getElementById("addButton");
 const list = document.getElementById("taskList");
 
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 button.addEventListener("click", addTask);
 
@@ -11,7 +11,15 @@ function addTask() {
 
     if (taskText === "") return;
 
-    tasks.push(taskText);
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false
+    };
+
+    tasks.push(newTask);
+
+    saveTasks(); 
 
     input.value = "";
 
@@ -21,10 +29,60 @@ function addTask() {
 function renderTasks() {
     list.innerHTML = "";
 
-    tasks.forEach(function(task) {
+    tasks.forEach(function(task, index) {
         const li = document.createElement("li");
-        li.textContent = task;
+
+        const span = document.createElement("span");
+        span.textContent = task.text;
+
+        span.addEventListener("click", function() {
+            toggleTask(task.id);
+        });
+
+
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "❌";
+
+        removeButton.addEventListener("click", function() {
+            removeTask(task.id);
+        });
+
+        li.appendChild(span);
+        li.appendChild(removeButton);
 
         list.appendChild(li);
-    })
+
+        if (task.done) {
+            span.style.textDecoration = "line-through";
+            span.style.opacity = "0.5";
+        }
+
+    });
 }
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
+function removeTask(id) {
+    tasks = tasks.filter(task => task.id !== id);
+
+    saveTasks();
+    renderTasks();
+}
+
+function toggleTask(id) {
+    tasks = tasks.map(task => {
+        if (task.id === id) {
+            return { ...task, done: !task.done };
+        }
+        return task;
+    });
+
+    saveTasks();
+    renderTasks();
+}
+
+
+renderTasks();
